b0ea0816c669916a7633c2411fda4fd17d451f03
mirasophia <mira3sophia.ikhsan@gmail.com>
2025-09-08 22:42:05 +0700

    Commit No. 2 – Result activity [Intent]

--- app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.lab_week_02_b
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputColor = findViewById<EditText>(R.id.inputColor)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+
+        btnSubmit.setOnClickListener {
+            val colorCode = inputColor.text.toString()
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("COLOR_CODE", colorCode)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -0,0 +1,24 @@
+package com.example.lab_week_02_b
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val layout = findViewById<ConstraintLayout>(R.id.resultLayout)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+
+        val colorCode = intent.getStringExtra("COLOR_CODE")
+
+        if (colorCode != null) {
+            layout.setBackgroundColor(Color.parseColor(colorCode))
+            txtResult.text = "Background changed to $colorCode"
+        }
+    }
+}
